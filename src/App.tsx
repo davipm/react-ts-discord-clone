d4d8fc4dead77c70fa273c1fe56de8122430755e
davi <davi.p.m94@gmail.com>
2020-12-19 19:06:14 -0300

    main app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Global from "./styles/global";
+
+import Layout from "./components/layout";
+
+export default function App() {
+  return (
+    <div id="app">
+      <Layout />
+      <Global />
+    </div>
+  );
+}
