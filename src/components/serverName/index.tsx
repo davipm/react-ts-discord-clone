eda461205bca6c5ce7bfa5fb96ac822bc3e1bd3b
davi <davi.p.m94@gmail.com>
2020-12-19 19:05:39 -0300

    adding server name

---
+++ src/components/serverName/index.tsx
@@ -0,0 +1,10 @@
+import { Container, ExpandIcon, Title } from "./styles";
+
+const ServerName = () => (
+  <Container>
+    <Title>David Server</Title>
+    <ExpandIcon />
+  </Container>
+);
+
+export default ServerName;
