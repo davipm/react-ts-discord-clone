c969791972a091e73d86e4149323fc7a19a3c702
davi <davi.p.m94@gmail.com>
2020-12-20 13:37:19 -0300

    adding macro

--- src/components/userInfo/styles.ts
+++ src/components/userInfo/styles.ts
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled, { css } from "styled-components/macro";
 import { Mic, Headset, Settings } from "styled-icons/material";
 
 const IconCSS = css`
